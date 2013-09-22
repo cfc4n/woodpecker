64ae57f2f644d78e5d01252b3f1407812de4d8bd
cnxct <cfc4n@cnxct.com>
2013-09-22 16:53:18 +0800

    Feature
    new feature: confirm the evil code with server cloud.

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
  * @license         http://www.fsf.org/copyleft/gpl.html GNU public license
  * @author          CFC4N <cfc4n@cnxct.com>
  * @package         demo
- * @version         $Id: index.php 17 2013-09-17 11:17:35Z cfc4n $
+ * @version         $Id: index.php 18 2013-09-22 08:47:57Z cfc4n $
  */
 set_time_limit(0);
 define('MAX_STRLEN', 500);    //max length value of hash string
@@ -64,14 +64,14 @@ try {
                     foreach ($line as $c)
                     {
                         $html1 .= 'line '.$c['line'].' :'.'<span class="code" title="'.$func.' '.htmlspecialchars($c['codemore']).'">'.$func.' ';
-                        $strLess = urlencode(base64_encode($func.$c['codeless']));
+                        $strLess = base64_encode($func.$c['codeless']);
                         if (strlen($strLess) > MAX_STRLEN)
                         {
-                            $html1 .= htmlspecialchars(substr($c['codemore'],0,50)).'</span><input type="hidden" hash_type="md5" value="'.md5($func.$c['codeless']).'"/><br/>';
+                            $html1 .= htmlspecialchars(substr($c['codemore'],0,50)).'</span><input type="hidden" value="md5" class="'.md5($func.$c['codeless']).'" title="'.$strLess.'"/><br/>';
                         }
                         else
                         {
-                            $html1 .= htmlspecialchars(substr($c['codemore'],0,50)).'</span><input type="hidden" hash_type="code" value="'.$strLess.'"/><br/>';
+                            $html1 .= htmlspecialchars(substr($c['codemore'],0,50)).'</span><input type="hidden" value="code" class="'.md5($func.$c['codeless']).'"  title="'.$strLess.'"/><br/>';
                         }
                     }
                     $html .='<td>'.$func.'</td> <td>'.$html1.'</td> <td align="center"> - </td></tr>';
